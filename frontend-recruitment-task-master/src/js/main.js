2b183a2d8cca430e3e020eb65ffe44acc7530afc
Balch95 <lukasz.wozniak95@hotmail.com>
2022-05-31 11:04:16 +0200

    js-popup-and-value

--- frontend-recruitment-task-master/src/js/main.js
+++ frontend-recruitment-task-master/src/js/main.js
@@ -1,5 +1,36 @@
 
-document.getElementById('button').addEventListener('click', ()=>{
-    document.getElementById('pop-up').classList.add('visibilty');
+//Add click value
+
+let clickValue = 0;
+
+const onClick = () =>{
+    clickValue++;
+    document.getElementById("counter").innerText = clickValue + ' times';
+    console.log(clickValue);
+}
+
+document.getElementById('button').onclick = onClick
+
+
+//Pop-up click
+
+document.getElementById("button").addEventListener("click", (e) => {
+    e.stopPropagation();
+    document.getElementById("pop-up").style.visibility = "visible";
+});
+  
+document.addEventListener("click", () => {
+    document.getElementById("pop-up").style.visibility = 'hidden';
+});
+  
+document.getElementById("pop-up").addEventListener("click", (e) => {
+    e.stopPropagation();
 });
 
+
+
+
+
+
+
+ 
