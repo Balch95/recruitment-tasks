d3e68767f52dd2d7381cbb87f0ff2544cd77e4fe
Balch95 <lukasz.wozniak95@hotmail.com>
2022-05-31 20:32:41 +0200

    js-clcik

--- frontend-recruitment-task-master/src/js/main.js
+++ frontend-recruitment-task-master/src/js/main.js
@@ -3,13 +3,48 @@
 
 let clickValue = 0;
 
+clickValue = localStorage.getItem('click')
+
 const onClick = () =>{
     clickValue++;
     document.getElementById("counter").innerText = clickValue + ' times';
-    console.log(clickValue);
+    return clickValue;
+};
+
+
+const setValue = () =>{
+    clickValue = onClick();
 }
 
-document.getElementById('button').onclick = onClick
+document.getElementById('button').onclick = setValue;
+
+// Reset button
+
+
+const buttonResetSet = (value) =>{
+    if(value >= 5){
+        document.getElementById('reset').style.visibility = 'visible';
+    }else{
+        document.getElementById("reset").style.visibility = 'hidden';
+    };
+        
+};
+
+const onClickResetValue = () => {
+    clickValue = 0;
+    localStorage.setItem('click', 0);
+    document.getElementById("counter").innerText = clickValue + ' times';
+    return clickValue;
+}
+
+const setValueReset = () =>{
+    clickValue = onClickResetValue();
+}
+
+document.getElementById('reset').addEventListener('click', setValueReset);
+
+
+
 
 
 //Pop-up click
@@ -17,15 +52,20 @@ document.getElementById('button').onclick = onClick
 document.getElementById("button").addEventListener("click", (e) => {
     e.stopPropagation();
     document.getElementById("pop-up").style.visibility = "visible";
+    buttonResetSet(clickValue);
+    localStorage.setItem('click', clickValue);
+    console.log(clickValue)
 });
   
 document.addEventListener("click", () => {
-    document.getElementById("pop-up").style.visibility = 'hidden';
+    document.getElementById("pop-up").style.visibility = 'hidden'
+    document.getElementById("reset").style.visibility = 'hidden';
 });
   
 document.getElementById("pop-up").addEventListener("click", (e) => {
     e.stopPropagation();
 });
+
 
 
 
